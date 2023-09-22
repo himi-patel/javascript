5f4ba5e929e1a193d16cbe320072110047b8f86e
himi-patel <himipatel9152@gmail.com>
2023-09-22 13:37:20 +0530

    first commit

---
+++ js(programs)/dc.js
@@ -0,0 +1,30 @@
+let d1=document.createElement("select")
+d1.id="drop"
+d1.style.width="150px"
+document.body.appendChild(document.createElemenyd1)
+
+let o1=document.createElement("option")
+o1.innerHTML="Blue"
+o1.value="Blue"
+d1.appendChild(o1)
+
+let o2=document.createElement("option")
+o2.innerHTML="Red"
+o2.value="Red"
+d1.appendChild(o2)
+
+let o3=document.createElement("option")
+o3.innerHTML="Green"
+o3.value="Green"
+d1.appendChild(o3)
+
+let o4=document.createElement("option")
+o4.innerHTML="Pink"
+o4.value="Pink"
+d1.appendChild(o4)
+
+let o5=document.createElement("option")
+o5.innerHTML="Orange"
+o5.value="Orange"
+d1.appendChild(o5)
+
